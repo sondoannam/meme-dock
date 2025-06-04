3d785dba404dfc2ac582c1ab830888e1d7684461
sondoannam <sondoannam202@gmail.com>
2025-06-04 21:38:31 +0700

    feat(app): :tada: create project with nx

---
+++ apps/cms/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+    return (
+        <div>
+            <NxWelcome title="@meme-dock/cms" />
+        </div>
+    );
+}
+
+export default App;
