06ff5a938aae0fe477f9fb0cb9c8098f45e5aee9
sondoannam <sondoannam202@gmail.com>
2025-06-19 17:14:56 +0700

    chore(app/api): test appwrite function

--- apps/api/src/appwrite-functions.ts
+++
@@ -1,3 +0,0 @@
-export default async ({ req, res, log, error }: any) => {
-  return res.send('Hello from Appwrite Functions!...');
-};
---
+++ apps/api/src/appwrite-functions/starter.ts
@@ -0,0 +1,21 @@
+export default async ({ req, res, log, error }: any) => {
+  log('Hello, Logs!');
+
+  // If something goes wrong, log an error
+  error('Hello, Errors!');
+
+  // The `req` object contains the request data
+  if (req.method === 'GET') {
+    // Send a response with the res object helpers
+    // `res.text()` dispatches a string back to the client
+    return res.text('Hello, World!');
+  }
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    motto: 'Build like a team of hundreds_ok!',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
+  });
+};
